81a1376075b1b7cd26233ce35ece04525fb86880
Nick Titov <titonick174@icloud.com>
2020-10-18 12:43:09 +0900

    Add finish line to splash activity

---
+++ app/src/main/java/ru/embersoft/animatedsplashscreen/SplashActivity.kt
@@ -0,0 +1,32 @@
+package ru.embersoft.animatedsplashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val motionLayout = findViewById<MotionLayout>(R.id.motionLayout)
+        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+
+        })
+    }
+}
